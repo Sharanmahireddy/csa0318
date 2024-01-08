a45747d41f327adaf3b8f14fc8a805516c69a29f
Sharanmahireddy <154489104+Sharanmahireddy@users.noreply.github.com>
2024-01-08 10:26:19 +0530

    Update insertion sort.cpp

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -18,6 +18,7 @@ void insertionSort(int arr[], int n)
 void printArray(int arr[], int n)
 {
 	int i;
+	printf("\nsorted array:\n");
 	for (i = 0; i < n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
@@ -26,6 +27,11 @@ int main()
 {
 	int arr[] = {12, 11, 13, 5, 6};
 	int n = sizeof(arr) / sizeof(arr[0]);
+	printf("array before sorting:\n");
+	for (int i=0;i<n;i++)
+		{
+		   printf("%d ",a[i];
+		}
 	insertionSort(arr, n);
 	printArray(arr, n);
 	return 0;
